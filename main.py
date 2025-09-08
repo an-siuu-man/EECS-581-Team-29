452b0deccc1e152566715b4654655d026f11c3b9
tahanurkhalid <tahanurkhalid@gmail.com>
2025-09-07 20:23:57 -0500

    Added dictionary

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 # --- Configuration ---
 WIDTH, HEIGHT = 10, 10
 
+def make_cell(r, c):
+    return {
+        "row": r,
+        "col": c,
+        "is_mine": False,
+        "revealed": False,
+        "flagged": False,
+        "adjacent": 0
+    }
+
 def create_board(width, height):
-    return [[{"revealed": False} for _ in range(width)] for _ in range(height)]
+    return [[make_cell(r, c) for c in range(width)] for r in range(height)]
 
 def print_board(board):
-    # fixed cell width
-    cell_width = max(len(str(WIDTH - 1)), 1) + 4  # extra padding
-
     # --- Column headers ---
-    header = " " * (len(str(HEIGHT - 1)) + 3)
-    for c in range(WIDTH):
-        header += str(c).center(cell_width)
+    header = " " * 4  # left padding
+    for c in range(len(board[0])):
+        header += str(c).center(3)
     print(header)
 
     # --- Rows ---
     for r, row in enumerate(board):
-        line = str(r).rjust(len(str(HEIGHT - 1))) + "  "
-        for _ in row:
-            content = "."
-            line += f"[ {content.center(cell_width-4)} ]"
+        line = str(r).rjust(3) + " "
+        for cell in row:
+            if cell["flagged"]:
+                symbol = "F"
+            elif not cell["revealed"]:
+                symbol = "."
+            elif cell["is_mine"]:
+                symbol = "*"
+            else:
+                symbol = str(cell["adjacent"])
+            line += symbol.center(3)
         print(line)
     print()
 
